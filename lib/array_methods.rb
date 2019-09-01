38e2fcebc67c7189ec9ef32eca61bba69638ae3b
Mark Stone <markwstone85@gmail.com>
2019-09-01 20:28:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
+# return index of value that is passed index
+# create loop that runs through array until it finds array provided
+
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  #array.index(value_to_find)
 end
 
 def find_max_value(array)
